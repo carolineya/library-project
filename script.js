7c22ab14648a54c41969334761ba0187bc030016
carolineya <caroline.yang@greatminds.org>
2024-10-23 20:26:58 -0400

    Add read state toggle button.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "read", "#64412a", "100", "350")
-const book2 = new Book("The Brief Wondrous Life of Oscar Wao", "Junot Diaz", 339, "not read yet", "#9d6838", "150", "450")
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "READ", "#64412a", "100", "350")
+const book2 = new Book("The Brief Wondrous Life of Oscar Wao", "Junot Diaz", 339, "NOT READ", "#9d6838", "150", "450")
 const bookColors = ["#64412a", "#9d6838", "#7a2e19", "#212121", "#0b2f3b",
      "#0f3a63", "#1180bc", "#9d4724", "#5f6a54", "#81231e", "#70683b", "#295128"]
 
 let myLibrary = [book1, book2];
 
 const bookListEl = document.getElementById("book-list")
-const addBookBtn = document.getElementById("submit")
+const addBookForm = document.getElementById("new-book-form")
 const bookContainerEl = document.getElementById("book-container")
 const showBtn = document.getElementById("show-dialog")
 
@@ -38,8 +38,14 @@ function drawLibrary(library) {
                     <hr>
                     <p id="author-text">${library[i].author}</p>
                     <p id="page-text">${library[i].pages} pages</p>
+                    <div class="read-marker" style="color:${library[i].color}">
+                        <p id="read-text">${library[i].read}</p>
+                    </div>
                 </div>
-                <button class="trash-btn" onclick="deleteBook(this.value)" value="${i}"><i class="fa fa-trash-o" style="font-size:26px"></i></button>
+                <div class="btns">
+                    <button class="read-btn" onclick="toggleReadState(this.value)" value="${i}" title="Change book to READ or NOT READ">&#128216</button>
+                    <button class="trash-btn" onclick="deleteBook(this.value)" value="${i}" title="Delete book"><i class="fa fa-trash-o" style="font-size:26px"></i></button>
+                </div>
             </div>`
     }
     bookContainerEl.innerHTML = ""
@@ -55,23 +61,21 @@ function getBookDim(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-addBookBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    dialog.close();
+addBookForm.addEventListener("submit", function() {
     let bookTitle = document.getElementById("book-title").value
     let bookAuthor = document.getElementById("book-author").value
     let bookPages = document.getElementById("book-pages").value
     let bookRead = document.querySelectorAll('input[type="checkbox"]:checked').length;
     if (bookRead) {
-        var newBook = new Book(bookTitle, bookAuthor, bookPages, "read",
+        var newBook = new Book(bookTitle, bookAuthor, bookPages, "READ",
             getCoverColor(), getBookDim(minWidth, maxWidth), getBookDim(minHeight, maxHeight)
         )
     } else {
-        var newBook = new Book(bookTitle, bookAuthor, bookPages, "not read yet",
+        var newBook = new Book(bookTitle, bookAuthor, bookPages, "NOT READ",
             getCoverColor(), getBookDim(minWidth, maxWidth), getBookDim(minHeight, maxHeight)
         )
     }
-
+    console.log(newBook)
     myLibrary.push(newBook)
     drawLibrary(myLibrary)
   });
@@ -79,6 +83,15 @@ addBookBtn.addEventListener("click", (e) => {
 showBtn.addEventListener("click", () => {
     dialog.showModal();
   });
+
+function toggleReadState(value) {
+    if(myLibrary[value].read === "NOT READ") {
+        myLibrary[value].read = "READ"
+    } else {
+        myLibrary[value].read = "NOT READ"
+    }
+    drawLibrary(myLibrary)
+  }
 
 function deleteBook(value) {
     myLibrary.splice(value, 1)
