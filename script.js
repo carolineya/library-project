e50fb2cd2968a45786c739c90a4c48aabc2c6708
carolineya <caroline.yang@greatminds.org>
2024-10-31 15:03:09 -0400

    Revised hover message on buttons and corrected author's name.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "READ", "#64412a", "100", "350")
-const book2 = new Book("The Brief Wondrous Life of Oscar Wao", "Junot Diaz", 339, "NOT READ", "#9d6838", "150", "450")
+const book2 = new Book("The Brief Wondrous Life of Oscar Wao", "Junot D\xEDaz", 339, "NOT READ", "#9d6838", "150", "450")
 const bookColors = ["#64412a", "#9d6838", "#7a2e19", "#212121", "#0b2f3b",
      "#0f3a63", "#1180bc", "#9d4724", "#5f6a54", "#81231e", "#70683b", "#295128"]
 
@@ -43,8 +43,8 @@ function drawLibrary(library) {
                     </div>
                 </div>
                 <div class="btns">
-                    <button class="read-btn" onclick="toggleReadState(this.value)" value="${i}" title="Change book to READ or NOT READ">&#128216</button>
-                    <button class="trash-btn" onclick="deleteBook(this.value)" value="${i}" title="Delete book"><i class="fa fa-trash-o" style="font-size:26px"></i></button>
+                    <button class="read-btn" onclick="toggleReadState(this.value)" value="${i}" title="Change READ status of book">&#128216</button>
+                    <button class="trash-btn" onclick="deleteBook(this.value)" value="${i}" title="Remove book from library"><i class="fa fa-trash-o" style="font-size:26px"></i></button>
                 </div>
             </div>`
     }
